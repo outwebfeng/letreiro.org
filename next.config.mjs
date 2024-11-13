f9189a027e75e84a6817ff9d25881d8df111d03c
outwebfeng <amylindsey958@gmail.com>
2024-11-13 12:30:16 +0800

    __dirname is not defined 问题处理。-3

--- next.config.mjs
+++ next.config.mjs
@@ -7,36 +7,6 @@ const nextConfig = {
   env: {
     NEXT_BASE_API: process.env.NEXT_BASE_API,
   },
-  logging: {
-    fetches: {
-      fullUrl: process.env.NODE_ENV === 'development',
-    },
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  images: {
-    unoptimized: true,
-  },
-  productionBrowserSourceMaps: false,
-  experimental: {
-    instrumentationHook: false,
-    serverComponentsExternalPackages: ['ua-parser-js'],
-    runtime: 'nodejs',
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default withNextIntl(nextConfig);
