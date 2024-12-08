fa5a1af562004e528eda306d34367b1dfd704c36
outwebfeng <amylindsey958@gmail.com>
2024-12-08 20:16:00 +0800

    添加 monetag js脚本。

--- components/ad/GoogleAdScript.tsx
+++ components/ad/GoogleAdScript.tsx
@@ -3,5 +3,16 @@ import Script from 'next/script';
 import { GOOGLE_ADSENSE_URL } from '@/lib/env';
 
 export default function GoogleAdScript() {
-  return <Script async src={GOOGLE_ADSENSE_URL} crossOrigin='anonymous' />;
+  return (
+    <>
+      <Script async src={GOOGLE_ADSENSE_URL} crossOrigin='anonymous' />
+      <Script
+        src='https://alwingulla.com/88/tag.min.js'
+        data-zone='118553'
+        async
+        data-cfasync='false'
+        strategy='afterInteractive'
+      />
+    </>
+  );
 }
