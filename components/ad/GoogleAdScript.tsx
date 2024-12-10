22ef7dd30a5e61507db1c0c0ae7c0a1699c39ddb
outwebfeng <amylindsey958@gmail.com>
2024-12-10 09:58:40 +0800

    去除 monetag 广告，非常影响体验

--- components/ad/GoogleAdScript.tsx
+++ components/ad/GoogleAdScript.tsx
@@ -3,16 +3,5 @@ import Script from 'next/script';
 import { GOOGLE_ADSENSE_URL } from '@/lib/env';
 
 export default function GoogleAdScript() {
-  return (
-    <>
-      <Script async src={GOOGLE_ADSENSE_URL} crossOrigin='anonymous' />
-      <Script
-        src='https://alwingulla.com/88/tag.min.js'
-        data-zone='118553'
-        async
-        data-cfasync='false'
-        strategy='afterInteractive'
-      />
-    </>
-  );
+  return <Script async src={GOOGLE_ADSENSE_URL} crossOrigin='anonymous' />;
 }
